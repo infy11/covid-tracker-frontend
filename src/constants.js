bf47fbc8875d31c91b6affc820b2010935ab11a2
varnit <varnitgoyal95@gmail.com>
2022-04-26 16:52:42 +0530

    change hostname

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://144.24.97.150:3000/getcoviddata';
-export const BASE_HOSTNAME = '144.24.97.150:3000';
+export const BASE_URL = 'https://backend.mohithairaffair.com/getcoviddata';
+export const BASE_HOSTNAME = 'https://backend.mohithairaffair.com';
